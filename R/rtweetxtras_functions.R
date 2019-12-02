4bad480e75be3c3451f4898a7c6cac51b71c3c91
Andrew Fraser <arf9999+gh@gmail.com>
2019-12-02 09:58:06 +0200

    added function: follower_number

--- R/rtweetxtras_functions.R
+++ R/rtweetxtras_functions.R
@@ -718,3 +718,31 @@ get_friends_fast <- function(account_for_friend, token_list = c(NULL), file_path
   }
   friend_df_users
 }
+
+
+####################################################################################
+#'@title Number followers and indicate earliest following date
+#'@description This function consolidates the process of numbering followers and indicating earliest following date.
+#'requires an rtweet dataframe as returned by a combination of get_followers() and lookup_users().
+#'@param follower_df Required. An rtweet dataframe of followers.
+#'#'@keywords twitter, rtweet
+#'@export
+#'@examples
+#'jacks_followers <- rtweet::lookup_users(unlist(rtweet::get_followers("jack", n=5000)[,"user_id"]))
+#'jacks_followers_numbered <- number_followers(jacks_followers)
+#######################################################################################
+
+number_followers <- function(follower_df){
+  require(dplyr, quietly = TRUE)
+  require(magrittr, quietly = TRUE)
+
+  follower_df <- follower_df %>%
+    mutate(follower_no = (dense_rank(desc(row_number()))))
+
+  follower_df$earliest_follow <- NULL
+  creation_dates <- follower_df$account_created_at
+  for (x in 1:nrow(follower_df)){
+    follower_df[x,"earliest_follow"] <- max(creation_dates[x:nrow(follower_df)])
+  }
+  return(follower_df)
+}
