47c33848c77004da69eb6796cc2aef1b2a860f9a
Andrew Fraser <arf9999+gh@gmail.com>
2020-09-21 16:17:31 +0200

    add hex sticker

---
+++ rtweetXtrashex.R
@@ -0,0 +1,6 @@
+p <- sticker("toolbox.png", package="rtweetXtras", p_size=7, s_x=1, s_y=.75, s_width=.6,
+        h_fill = "#BCD2EE", h_color = "#473C8B",  filename="rtweetXtrashex.png")
+
+
+plot(p)
+
