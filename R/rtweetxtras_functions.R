3cfdee49409e9a1721e625cd63024b90c35090d0
Andrew Fraser <arf9999+gh@gmail.com>
2020-10-14 12:13:54 +0200

    Add snscrape historical search

--- R/rtweetxtras_functions.R
+++ R/rtweetxtras_functions.R
@@ -1350,3 +1350,71 @@ got_search <-
 
 
   }
+
+#############################################################################
+#'@title Wrapper to undertake historical searches calling Python snscrape library.
+#'@param search_string A search string (in quotes)
+#'@param since_date Start date for the search (iso format date in quotes)
+#'@param until_date Latest date for the search (iso formatdate in quotes NB: search works backward)
+#'@param n Maximum number of results - twitter API has 90k rate-limit per 15 minutes
+#'@param file temporary file name for snscrape URL list, timestamp will be appended.
+#'@param token An OAuth token loaded in the environment for twitter's Standard API (if not specified, default token will be used)
+#'@param token_list A list of OAuth tokens loaded in the environment (REQUIRED if token is not specified)
+#'@param delete_tempfile Clear temp file of statuses default = TRUE
+#'@description Calls Python script to pull staus URLs of a search, rtweet to rehdrate those statuses.
+#'            See https://github.com/JustAnotherArchivist/snscrape
+#'@keywords twitter, snscrape, rtweet
+#'@export
+#'@examples
+#'test <- snscrape_search("Trump", since_date = "2016-09-06", until_date = "2016-11-06", n = 1000, file = "test_", token = my_token)
+###############################################################################
+
+function (search_string, #search terms in quotes
+          since_date = NULL, #optional iso date in quotes
+          until_date = NULL, #optional iso date in quotes
+          n = 100, #max number of statuses to retrieve. Fewer than 90K recommend due to rate-limiting
+          file = "temp", #temporary name for snscrape text file
+          token = NULL, #specify token if required
+          delete_tempfile = TRUE) #delete text file od statuses
+{
+  require(rtweet, quietly = TRUE)
+  require(readr, quietly = TRUE)
+  require(dplyr, quietly = TRUE)
+
+  if(!is.null(since_date)) {
+    search_string <- paste0(search_string," since:",since_date)
+  }
+
+  if(!is.null(until_date)){
+    search_string <-paste0(search_string, " until:", until_date)
+  }
+
+  output_path <- paste0(file, as.numeric(Sys.time()), ".txt")
+
+  ##call Python scraper
+  system(paste0("snscrape -n ", n," twitter-search ","\"", search_string,"\"",
+                " > ", output_path))
+
+  ##import status_ids from text file
+  scrape_statuses <-  read_delim(output_path, "/", escape_double = FALSE,
+                                 col_names = FALSE,
+                                 col_types = cols(X2 = col_skip(),
+                                                  X1 = col_skip(), X3 = col_skip(),
+                                                  X5 = col_skip(), X6 = col_character()),
+                                 trim_ws = TRUE) %>%
+    dplyr::rename(screen_name = X4,
+                  status_id = X6) %>%
+    dplyr::distinct(status_id, .keep_all = TRUE)
+
+  message(paste(nrow(scrape_statuses), "status URLS captured, rehydrating..."))
+  temp_rehydration <- rtweet::lookup_statuses(scrape_statuses$status_id, token = token)
+
+ ##cleanup temp files
+   if (delete_tempfile == TRUE & file.exists(output_path)){
+    file.remove(output_path)
+  }
+
+  return(temp_rehydration)
+
+}
+
