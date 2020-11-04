7b88661de33dfc30afed1e058e0e18d419ac1ad7
Andrew Fraser <arf9999+gh@gmail.com>
2020-11-04 21:33:22 +0200

    Update get_followers_fast and get_friends_fast - correct issues with small followings/friendships

--- R/rtweetxtras_functions.R
+++ R/rtweetxtras_functions.R
@@ -540,12 +540,13 @@ save_csv_edgelist <- function(igraphobject, path) {
 
 #################################################################################
 #'@title Wrapper for rtweet::get_followers and rtweet::lookup_users
-#'@description This function consolidates the process of getting followers and looking up the user details.
+#'@description This function consolidates the process of getting followers and looking up the user details,
+#'It also adds a column for follower number, the earliest follow date and the account screen_name being followed.
 #'@param account_for_foll Required. The target twitter account user_id or screen_name - in quotes.
 #'NB:Only tested with a single account: iterating tokens may not work effectively with multiple accounts,
 #'however, using lapply or purrr::map to iterate over user accounts is feasible.
 #'@param token_list Optional. A list of valid tokens loaded into environment. Not specifying will use the default token.
-#'If multiple tokens are specified, function will iterate through them to reduce ratelimit pauses for followings larger than 75000
+#'If multiple tokens are specified, function will iterate through them to reduce rate-limit pauses for followings larger than 75000
 #'NB: this may be contrary to Twitter's API terms and conditions - so use at own risk.
 #'@param file_path Optional. If specified, an rds file will be saved to the file path in the form <file_path><account_for_foll>_followers.rds
 #'#'@keywords twitter, rtweet
@@ -560,6 +561,9 @@ get_followers_fast <-
     require(rtweet, quietly = TRUE)
     require(dplyr, quietly = TRUE)
     require(purrr, quietly = TRUE)
+    require(lubridate, quietly = TRUE)
+
+    message("For accounts with a large following, this process can take a (very) long time.\nPlease be patient.")
 
     followers_count <-
       as.numeric(tryCatch(
@@ -607,13 +611,15 @@ get_followers_fast <-
       return(c(cond))
       page <- 0
     })
+
     page <- next_cursor(follower_df) ## set cursor for paging
-    print(paste("page = ", page))
-    follower_df_users <- ifelse(!is.na(follower_df$user_id),
-                                bind_rows(
-                                  lookup_users(follower_df$user_id, parse = TRUE, token = token_list[tokencount])
-                                ),
-                                c(NA))
+    message(paste("Page reference > 0 means more than a single pass required\n Page reference = ", page))
+
+    follower_df_users <- if (!is.na(follower_df[1,1])){
+      rtweet::lookup_users(follower_df$user_id, parse = TRUE, token = token3)
+    }else{
+      NA
+    }
 
 
     if (!is.null(file_path)) {
@@ -646,7 +652,7 @@ get_followers_fast <-
 
 
       follower_df_users <- follower_df_users %>%
-        rbind(follower_df_temp_user)
+        dplyr::bind_rows(follower_df_temp_user)
 
 
       if (!is.null(file_path)) {
@@ -663,11 +669,37 @@ get_followers_fast <-
         "\n"
       ))
     }
-    follower_df_users
+    followed_acc_user_creation <- rtweet::lookup_users(account_for_foll, token = token_list[tokencount]) %>%
+      select(account_created_at) %>%
+      as.numeric() %>%
+      unname() %>%
+      unlist() %>%
+      lubridate::as_datetime()
+
+
+
+    follower_df_users <- follower_df_users %>%
+      dplyr::mutate(follower_of = account_for_foll) %>%
+      dplyr::relocate(follower_of) %>%
+      rtweetXtras::number_followers() %>%
+      mutate(earliest_follow = case_when (earliest_follow < followed_acc_user_creation ~
+                                            followed_acc_user_creation,
+                                          TRUE ~ earliest_follow)
+      )
+
+
+    if (!is.null(file_path)) {
+      saveRDS(follower_df_users,
+              paste0(file_path, account_for_foll, "_followers.rds"))## save rds file of followers
+    }
+
+    return(follower_df_users)
   }
+
 ####################################################################################
 #'@title Wrapper for rtweet::get_friends and rtweet::lookup_users
 #'@description This function consolidates the process of getting friends and looking up the user details.
+#'Additional columns are created for account friendship reference, friend_no, and  earliest friendship date.
 #'@param account_for_friend Required. The target twitter account user_id or screen_name - in quotes.
 #'NB:Only tested with a single account: iterating tokens may not work effectively with multiple accounts,
 #'however, using lapply or purrr::map to iterate over user accounts is feasible.
@@ -683,10 +715,11 @@ get_followers_fast <-
 get_friends_fast <-
   function(account_for_friend,
            token_list = c(NULL),
-           file_path = "~/") {
+           file_path = NULL) {
     require(rtweet, quietly = TRUE)
     require(dplyr, quietly = TRUE)
     require(purrr, quietly = TRUE)
+    require(lubridate, quietly = TRUE)
 
     friend_count <-
       as.numeric(tryCatch(
@@ -737,11 +770,11 @@ get_friends_fast <-
     page <-
       ifelse(page != 0, next_cursor(friend_df), 0)  ## set cursor for paging
     print(paste("page = ", page))
-    friend_df_users <- ifelse(!is.na(friend_df$user_id),
-                              bind_rows(
-                                lookup_users(friend_df$user_id, parse = TRUE, token = token_list[tokencount])
-                              ),
-                              c(NA))
+    friend_df_users <- if(!is.na(friend_df[1,2])){
+      lookup_users(friend_df$user_id, parse = TRUE, token = token_list[tokencount])
+    }else{
+      NA
+    }
 
     if (!is.null(file_path)) {
       saveRDS(friend_df_users,
@@ -761,11 +794,12 @@ get_friends_fast <-
         token = token_list[tokencount]
       )
       friend_df_temp_user <-
-        lookup_users(follower_df_temp$user_id, token = token_list[tokencount])
+        lookup_users(friend_df_temp$user_id, token = token_list[tokencount])
 
-      friend_df <- rbind(follower_df, friend_df_temp)
+      friend_df <- bind_rows(friend_df, friend_df_temp)
       friend_df_users <-
         bind_rows(friend_df_users, friend_df_temp_user)
+
       if (!is.null(file_path)) {
         saveRDS(friend_df_users,
                 paste0(file_path, account_for_friend, "_friends.rds"))
@@ -779,13 +813,39 @@ get_friends_fast <-
         "\n"
       ))
     }
+
+    friend_acc_user_creation <- rtweet::lookup_users(account_for_friend, token = token_list[tokencount]) %>%
+      select(account_created_at) %>%
+      as.numeric() %>%
+      unname() %>%
+      unlist() %>%
+      lubridate::as_datetime()
+
+
+    friend_df_users <- friend_df_users %>%
+      mutate(friend_of = account_for_friend) %>%
+      rtweetXtras::number_followers() %>%
+      rename(friend_no = follower_no, earliest_friendship = earliest_follow) %>%
+      mutate(earliest_friendship = case_when (earliest_friendship < friend_acc_user_creation ~
+                                                friend_acc_user_creation,
+                                              TRUE ~ earliest_friendship)
+      ) %>%
+      relocate(friend_of)
+
+    if (!is.null(file_path)) {
+      saveRDS(friend_df_users,
+              paste0(file_path, account_for_friend, "_friends.rds"))
+    }
+
     friend_df_users
   }
+
 
 
 ####################################################################################
 #'@title Number followers and indicate earliest following date
 #'@description This function consolidates the process of numbering followers and indicating earliest following date.
+#'WARNING: Obviously not accurate for dates prior to the creation of the target account - manual adjustment required.
 #'requires an rtweet dataframe as returned by a combination of get_followers() and lookup_users().
 #'@param follower_df Required. An rtweet dataframe of followers.
 #'@keywords twitter, rtweet
